074ad41f0bffff457141788c4f07f250b109d511
Uma852 <kondauma95@gmail.com>
2022-05-27 16:04:48 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,5 @@
+var sum=5;
+var num=8;
+var res=sum+num;
+
+console.log("res")
---
+++ index.js
@@ -0,0 +1,17 @@
+var age=23+23;
+var sum=15-10;
+var msg="10/3 with a rem of "+sum;
+
+console.log(msg);
+console.log(age);
+var myba=300;
+var nikeshoes=784.76;
+if (nikeshoes<=myba)
+{
+    myba-=nikeshoes;
+    console.log("acc ba"+myba);
+    console.log("we should see this");
+}
+else{
+    console.log("nope");
+}
